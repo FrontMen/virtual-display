704da7fd7226663428ff904e9a075c84808bef8c
justusromijn <justus.romijn@gmail.com>
2017-07-07 09:39:16 +0200

    remove mongo shit: start with local storage for dev

--- index.js
+++ index.js
@@ -1,31 +1,13 @@
-const MongoClient = require("mongodb").MongoClient;
-const MongoCredentials = require("./config/mongo-credentials");
+const express = require("express");
 
-const uri = `mongodb://${MongoCredentials.username}:${MongoCredentials.password}@virtualdisplay1-shard-00-00-lovqf.mongodb.net:27017,virtualdisplay1-shard-00-01-lovqf.mongodb.net:27017,virtualdisplay1-shard-00-02-lovqf.mongodb.net:27017/${MongoCredentials.database}?ssl=true&replicaSet=VirtualDisplay1-shard-0&authSource=admin`;
-
-let testData = null;
-
-MongoClient.connect(uri, function(err, db) {
+const app = express();
 
 
-    initMongo(db);
-
+app.get("/status", (req, res) => {
+    res.send("Status call");
 });
 
-function initMongo(db){
-    db.listCollections({ name: "foo" }).toArray((err, items) => {
-        if (items[0]){
-            testData = items[0];
-            console.log(testData);
-            db.close();
-        } else {
-            createFoo(db);
-        }
-    });
-}
 
-function createFoo(db){
-    db.createCollection("foo", function(err, result){
-        initMongo(db);
-    });
-}
+app.listen(3000, () => {
+    console.log("App listening on port 3000.");
+});
